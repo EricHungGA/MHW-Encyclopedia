e9f4e92be9abda09b407c8bcb2e7c170652322d6
EricHungGA <ehung303@gmail.com>
2023-05-05 23:07:58 -0700

    completing the adding of authentification signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 def home(request):
     return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'Invalid Hunter Regisration Info - try again'
+    
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
