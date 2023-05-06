dacb4d65e571497581be904ac89c2e1211dfadc7
EricHungGA <ehung303@gmail.com>
2023-05-06 14:38:15 -0700

    importing requests and successful api testing and get requests

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+import requests
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
 
 def home(request):
-    return render(request, 'home.html')
+    response=requests.get('https://mhw-db.com/monsters/2').json()
+    all_monsters=requests.get('https://mhw-db.com/monsters').json()
+    return render(request, 'home.html', {'monster_1': response, 'all_monsters': all_monsters})
 
 def signup(request):
     error_message = ''
